2572ce3b7ace5f08a50d7d4ea7a81745d66b015a
chethanaautomation <cchethan65@gmail.com>
2025-02-18 15:22:32 +0530

    second commit

---
+++ src/test/java/chethanaauto/Reversestring.java
@@ -0,0 +1,14 @@
+package chethanaauto;
+
+public class Reversestring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s="hello";
+for (int i=s.length()-1; i>=0;i--)
+{
+	System.out.print(s.charAt(i));
+}
+	}
+
+}
